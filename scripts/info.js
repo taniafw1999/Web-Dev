c59c7112369902f2fbc46f2c63bfeab681faec38
Lan Le <m.t.n.p.l.le@students.uu.nl>
2020-03-02 13:20:16 +0100

    info start with basis course and teacher class (lame inheritance of named class)

---
+++ scripts/info.js
@@ -0,0 +1,42 @@
+
+class Named {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Teacher extends Named {
+    constructor(name) {
+        super(name);
+    }
+}
+
+const teacher = new Teacher(
+    "Sergey Sosnovsky"
+)
+
+class Course extends Named {
+    constructor(name, code, points, period, slot, teacher) {
+        super(name);
+      this.code = code;
+      this.points = points;
+      this.period = period;
+      this.slot = slot;
+      this.teacher = teacher;
+    }
+}
+
+const course = new Course(
+      "INFOB2WT"
+    , 7.5
+    , 3
+    , "D"
+    , teacher
+)
+
+
+
+// fill page
+document.getElementById("teacher__name").innerHTML = teacher.name;
+document.getElementById("course__name").innerHTML = course.name;
+document.getElementById("test").innerHTML = "test succeeded";
