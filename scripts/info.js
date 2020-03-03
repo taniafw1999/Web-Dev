9310e6144a1c43320a3d69d4bd8b41a88deb6e20
Lan Le <m.t.n.p.l.le@students.uu.nl>
2020-03-03 15:40:47 +0100

    actual course info

--- scripts/info.js
+++ scripts/info.js
@@ -16,18 +16,21 @@ const teacher = new Teacher(
 )
 
 class Course extends Named {
-    constructor(name, code, points, period, slot, teacher) {
+    constructor(name, department, code, points, period, slot, teacher) {
         super(name);
-      this.code = code;
-      this.points = points;
-      this.period = period;
-      this.slot = slot;
-      this.teacher = teacher;
+        this.department = department;
+        this.code = code;
+        this.points = points;
+        this.period = period;
+        this.slot = slot;
+        this.teacher = teacher;
     }
 }
 
 const course = new Course(
-      "INFOB2WT"
+      "Webtechnology"
+    , "Information and Computing Sciences"
+    , "INFOB2WT"
     , 7.5
     , 3
     , "D"
@@ -37,6 +40,12 @@ const course = new Course(
 
 
 // fill page
-document.getElementById("teacher__name").innerHTML = teacher.name;
+document.getElementById("course__department").innerHTML = course.department;
+document.getElementById("teacher__name").innerHTML = course.teacher.name;
 document.getElementById("course__name").innerHTML = course.name;
+document.getElementById("course__code").innerHTML = course.code;
+document.getElementById("course__points").innerHTML = course.points.toString();
+document.getElementById("course__period").innerHTML = course.period.toString();
+document.getElementById("course__slot").innerHTML = course.slot;
+document.getElementById("course__schedule").innerHTML = "<a href=\"schedule.html\">Schedule</a>";
 document.getElementById("test").innerHTML = "test succeeded";
